3ee433f3a735e1f033f8e0e19024e4fc2642cf08
Pablo Bru <pablobru08@gmail.com>
2024-10-17 12:31:49 -0300

    se realizaron los test

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +7,11 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  const user = new User("Pablo");
+  const nuevoProducto = new Product("fruta", 10);
+  user.addProduct(nuevoProducto)
+  t.is(user.name, "Pablo");
+  t.is(user.products[0].name, "fruta");
+  t.is(user.products[0].price, 10);
+});
